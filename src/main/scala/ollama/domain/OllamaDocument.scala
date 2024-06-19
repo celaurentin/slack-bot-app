b0edfc8c451d596426d586371e72fc6259423c0d
Carlos Daniel Canon Carrero <devcdcc@gmail.com>
2024-06-19 14:54:02 -0500

    tags as optional

--- src/main/scala/ollama/domain/OllamaDocument.scala
+++ src/main/scala/ollama/domain/OllamaDocument.scala
@@ -6,29 +6,29 @@ import zio.json.*
 import zio.schema.*
 
 case class Tag(
-  name: String
+    name: String
 )
 object Tag {
-  given schema: Schema[Tag] = DeriveSchema.gen[Tag]
+  given schema: Schema[Tag]       = DeriveSchema.gen[Tag]
   given jsonCodec: JsonCodec[Tag] = DeriveJsonCodec.gen[Tag]
 }
 
 case class Content(
-  tags: Chunk[Tag]
+    tags: Option[Chunk[Tag]]
 )
 
 object Content {
-  given schema: Schema[Content] = DeriveSchema.gen[Content]
+  given schema: Schema[Content]       = DeriveSchema.gen[Content]
   given jsonCodec: JsonCodec[Content] = DeriveJsonCodec.gen[Content]
 }
 
 case class OllamaDocument(
-  collection_name: String,
-  name: String,
-  title: String,
-  filename: String,
-  user_id: String,
-  content: Content
+    collection_name: String,
+    name: String,
+    title: String,
+    filename: String,
+    user_id: String,
+    content: Content
 )
 object OllamaDocument {
   given schema: Schema[OllamaDocument]       = DeriveSchema.gen[OllamaDocument]
