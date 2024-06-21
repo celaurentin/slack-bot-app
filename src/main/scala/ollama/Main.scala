8abc8e394d4caa2935a13c2d2b81a1fc9cc1f7a1
Carlos Daniel Canon Carrero <devcdcc@gmail.com>
2024-06-21 00:53:27 -0500

    update OLlamaAdapter, implement a token refresher

--- src/main/scala/ollama/Main.scala
+++ src/main/scala/ollama/Main.scala
@@ -9,10 +9,10 @@ import zio.http.*
 object Main extends ZIOAppDefault:
   private val app =
     for {
-      authorization <- OLlamaAdapter.auth(AuthRequest("cesar.laurentin@ninjatrader.com", "Arepa123++"))
-      documents     <- OLlamaAdapter.getDocuments(authorization.token)
-      chats         <- OLlamaAdapter.getChats(authorization.token)
-      answers       <- OLlamaAdapter.executePrompt(authorization.token, buildPromptRequest(chats, "define FCM in less than 20 words", documents))
+      authorization <- OLlamaAdapter.startSessionManger
+      documents     <- OLlamaAdapter.getDocuments
+      chats         <- OLlamaAdapter.getChats
+      answers       <- OLlamaAdapter.executePrompt(buildPromptRequest(chats, "define FCM in less than 20 words", documents))
       _             <- Console.ConsoleLive.printLine(answers)
     } yield ()
 
--- src/main/scala/ollama/adapters/OllamaAdapter.scala
+++ src/main/scala/ollama/adapters/OllamaAdapter.scala
@@ -13,23 +13,22 @@ import zio.json.*
 
 trait OLlamaAdapter {
   def auth(authRequest: AuthRequest): IO[OLlamaError, AuthResponse]
-  def getDocuments(bearerToken: String): IO[OLlamaError, Chunk[OllamaDocument]]
-  def getChats(bearerToken: String): IO[OLlamaError, Chunk[OllamaChat]]
-  def executePrompt(bearerToken: String, promptRequest: PromptRequest): IO[OLlamaError, PromptResponse]
+  def getDocuments: IO[OLlamaError, Chunk[OllamaDocument]]
+  def getChats: IO[OLlamaError, Chunk[OllamaChat]]
+  def executePrompt(promptRequest: PromptRequest): IO[OLlamaError, PromptResponse]
+  def startSessionManger: IO[OLlamaError, Unit]
 }
 
 object OLlamaAdapter {
-  private case class OLlamaAdapterLive(client: Client) extends OLlamaAdapter {
+  private case class OLlamaAdapterLive(client: Client, token: Ref[String]) extends OLlamaAdapter {
     private val url             = "http://localhost:3000"
     private val followRedirects = client @@ ZClientAspect.followRedirects(3)((resp, message) => ZIO.logInfo(message).as(resp))
 
-    override def getDocuments(bearerToken: String): IO[OLlamaError, Chunk[OllamaDocument]] = {
+    override def getDocuments: IO[OLlamaError, Chunk[OllamaDocument]] = {
       val documentsURL = s"$url/api/v1/documents"
-      val request = Request
-        .get(documentsURL)
-        .addHeader(Header.Authorization.Bearer(bearerToken))
       for {
-
+        bearerToken <- token.get
+        request = Request.get(documentsURL).addHeader(Header.Authorization.Bearer(bearerToken))
         response <- followRedirects.request(request).mapError(e => ConnectionOLlamaError(e.toString))
         body     <- response.body.asString.mapError(e => DeserializationOLlamaError(e.toString))
         _        <- Console.ConsoleLive.printLine(s"getDocuments status response: ${response.status} ${response.status.code}").ignore
@@ -37,20 +36,18 @@ object OLlamaAdapter {
       } yield response
     }.provide(Scope.default)
 
-    override def getChats(bearerToken: String): IO[OLlamaError, Chunk[OllamaChat]] = {
+    override def getChats: IO[OLlamaError, Chunk[OllamaChat]] = {
       val chatsURL = s"$url/api/v1/chats"
-      val request = Request
-        .get(chatsURL)
-        .addHeader(Header.Authorization.Bearer(bearerToken))
       for {
-
+        bearerToken <- token.get
+        request = Request.get(chatsURL).addHeader(Header.Authorization.Bearer(bearerToken))
         response <- followRedirects.request(request).mapError(e => ConnectionOLlamaError(e.toString))
-        body <- response.body.asString.mapError(e => DeserializationOLlamaError(e.toString))
-        _ <- Console.ConsoleLive.printLine(s"getChats status response: ${response.status} ${response.status.code}").ignore
+        body     <- response.body.asString.mapError(e => DeserializationOLlamaError(e.toString))
+        _        <- Console.ConsoleLive.printLine(s"getChats status response: ${response.status} ${response.status.code}").ignore
         response <- ZIO.fromEither(body.fromJson[Chunk[OllamaChat]]).mapError(e => DeserializationOLlamaError(e.toString))
       } yield response
     }.provide(Scope.default)
-    
+
     def auth(authRequest: AuthRequest): IO[OLlamaError, AuthResponse] = {
       val authURL = s"$url/api/v1/auths/signin"
       val request = Request.post(authURL, Body.from(authRequest))
@@ -61,12 +58,12 @@ object OLlamaAdapter {
       } yield response
     }.provide(Scope.default)
 
-    def executePrompt(bearerToken: String, promptRequest: PromptRequest): IO[OLlamaError, PromptResponse] = {
+    def executePrompt(promptRequest: PromptRequest): IO[OLlamaError, PromptResponse] = {
       val promptURL = s"$url/ollama/api/chat"
-      val request   = Request
-        .post(promptURL, Body.from(promptRequest))
-        .addHeader(Header.Authorization.Bearer(bearerToken))
+
       for {
+        bearerToken <- token.get
+        request = Request.post(promptURL, Body.from(promptRequest)).addHeader(Header.Authorization.Bearer(bearerToken))
         response <- client.request(request).mapError(e => ConnectionOLlamaError(e.toString))
         _        <- Console.ConsoleLive.printLine(s"executePrompt status response: ${response.status} ${response.status.code}").ignore
         _        <- Console.ConsoleLive.printLine(s"executePrompt body: ${response.body.asString}").ignore
@@ -74,13 +71,38 @@ object OLlamaAdapter {
         response <- ZIO.fromEither(body.fromJson[PromptResponse]).mapError(e => DeserializationOLlamaError(e.toString))
       } yield response
     }.provide(Scope.default)
+
+    // startSessionManger is aiming to request our authentication token and store it in the Ref
+    // so we can use it in the other methods
+    // the token should be refreshed every hour
+    private def startSessionManger0: IO[OLlamaError, Unit] =
+      for {
+        authorization <- auth(AuthRequest("cesar.laurentin@ninjatrader.com", "Arepa123++")).retry(Schedule.spaced(1.minutes).forever)
+        _             <- token.set(authorization.token)
+      } yield ()
+    override def startSessionManger: IO[OLlamaError, Unit] = for {
+      _ <- startSessionManger0
+      _ <- startSessionManger0
+        .retry(Schedule.spaced(1.minutes).forever)
+        .repeat(Schedule.spaced(1.hour).forever)
+        .delay(1.hour)
+        .fork
+    } yield ()
+
   }
 
-  def auth(authRequest: AuthRequest): ZIO[OLlamaAdapter, OLlamaError, AuthResponse]                = ZIO.serviceWithZIO(_.auth(authRequest))
-  def getDocuments(bearerToken: String): ZIO[OLlamaAdapter, OLlamaError, Chunk[OllamaDocument]]    = ZIO.serviceWithZIO(_.getDocuments(bearerToken))
-  def getChats(bearerToken: String): ZIO[OLlamaAdapter, OLlamaError, Chunk[OllamaChat]]    = ZIO.serviceWithZIO(_.getChats(bearerToken))
-  def executePrompt(bearerToken: String, promptRequest: PromptRequest): ZIO[OLlamaAdapter, OLlamaError, PromptResponse] = ZIO.serviceWithZIO(_.executePrompt(bearerToken, promptRequest))
+  def auth(authRequest: AuthRequest): ZIO[OLlamaAdapter, OLlamaError, AuthResponse] = ZIO.serviceWithZIO(_.auth(authRequest))
+  def getDocuments: ZIO[OLlamaAdapter, OLlamaError, Chunk[OllamaDocument]]          = ZIO.serviceWithZIO(_.getDocuments)
+  def getChats: ZIO[OLlamaAdapter, OLlamaError, Chunk[OllamaChat]]                  = ZIO.serviceWithZIO(_.getChats)
+  def executePrompt(promptRequest: PromptRequest): ZIO[OLlamaAdapter, OLlamaError, PromptResponse] =
+    ZIO.serviceWithZIO(_.executePrompt(promptRequest))
+  def startSessionManger: ZIO[OLlamaAdapter, OLlamaError, Unit] = ZIO.serviceWithZIO(_.startSessionManger)
 
-  lazy val live: ZLayer[Client, Nothing, OLlamaAdapter]   = ZLayer.fromFunction(OLlamaAdapterLive(_))
-  lazy val default: ZLayer[Any, Throwable, OLlamaAdapter] = Client.default >>> ZLayer.fromFunction(OLlamaAdapterLive(_))
+  lazy val live: ZLayer[Client, Nothing, OLlamaAdapter] = ZLayer.fromZIO {
+    for {
+      client <- ZIO.service[Client]
+      ref    <- Ref.make("")
+    } yield OLlamaAdapterLive(client, ref)
+  }
+  lazy val default: ZLayer[Any, Throwable, OLlamaAdapter] = Client.default >>> live
 }
